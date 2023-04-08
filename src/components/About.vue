7bc0d713045f154d0cdc80de21894abffa7ec062
Zach Rickers <mzrickers@gmail.com>
2023-04-07 22:55:49 -0600

    router

---
+++ src/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="page">
+        <h1>About</h1>
+        <p>Your one stop drawing app</p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="page">
+        <h1>Home</h1>
+        <h2>Sketching</h2>
+        <h2>Inking</h2>
+        <h2>Coloring</h2>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
